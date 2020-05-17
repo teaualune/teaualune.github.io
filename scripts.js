e4890c04015f6a0702fb22c37260892b10bcf806
teaualune <teaualune@gmail.com>
2020-05-17 13:21:33 +0800

    fix script; update indents

--- scripts.js
+++ scripts.js
@@ -1,27 +1,24 @@
-(function () {
-var works = document.querySelectorAll('#works li');
+(function() {
+var works = document.querySelectorAll('#works .work');
 var tags = new Array(works.length);
 var sel = document.getElementById('sel-work');
 var w = 0;
 for (w = 0; w < works.length; w += 1) tags[w] = works[w].getElementsByTagName('span');
-window.filterWork = function () {
-    filter(sel.options[sel.selectedIndex].value);
-};
-function filter (tag) {
-    var work, t, matched;
-    for (w = 0; w < works.length; w += 1) {
-        work = works[w];
-        if (!tag || !tag.length) {
-            work.hidden = false;
-            continue;
-        }
-        matched = false;
-        for (t in tags[w]) if (tags[w][t].className === tag) {
-            matched = true;
-            break;
-        }
-        if (matched) work.hidden = false;
-        else work.hidden = true;
+window.filterWork = function() {
+  var tag = sel.options[sel.selectedIndex].value;
+  var work, t, matched;
+  for (w = 0; w < works.length; w += 1) {
+    work = works[w];
+    if (!tag || !tag.length) {
+      work.hidden = false;
+      continue;
     }
-}
+    matched = false;
+    for (t in tags[w]) if (tags[w][t].className === tag) {
+      matched = true;
+      break;
+    }
+    work.hidden = !matched
+  }
+};
 }());
